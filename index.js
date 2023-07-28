9781e690f35d684a72e348be0f3763e1b2d1355d
avinash_singh <avinash.singh@blinkx.in>
2023-07-28 22:47:57 +0530

    db

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Import the Express library
 const express = require('express');
 
+const mysql = require('mysql');
+  
+
 // Create an instance of the Express application
 const app = express();
 
@@ -11,5 +14,20 @@ app.get('/', (req, res) => {
 
 // Start the server on port 3000
 app.listen(3000, () => {
+    const connection = getConnection();
   console.log('Server is running on http://localhost:3000');
 });
+
+
+function getConnection ()
+{
+  const options = 
+  {  
+    
+    host: "35.200.239.13", //IP address of my Cloud SQL Server
+    user: 'root',
+    password: 'brainycrusader',
+    database: 'brainycrusader'
+  };
+  return mysql.createConnection(options);
+}
